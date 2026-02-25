de167c906971bc7e38969fd6327a76765bfa6dd1
shadowsama41ns-cpu <shadowsama41ns@gmail.com>
2026-02-25 14:16:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 from telethon import TelegramClient
+from telethon.sessions import StringSession
 from config import API_ID, API_HASH, STRING_SESSION
 from commands import register_handlers
 from core.queue import worker
@@ -7,12 +8,15 @@ from core.queue import worker
 
 async def main():
     client = TelegramClient(
-        STRING_SESSION,
+        StringSession(STRING_SESSION),
         API_ID,
         API_HASH
     )
 
-    await client.start()
+    await client.connect()
+
+    if not await client.is_user_authorized():
+        raise Exception("STRING_SESSION inválida!")
 
     print("Userbot online...")
 
