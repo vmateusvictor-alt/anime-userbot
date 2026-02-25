cf675739f9857823a58a317877ed9ac0ea09d25f
shadowsama41ns-cpu <shadowsama41ns@gmail.com>
2026-02-25 12:02:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
+import asyncio
 from telegram.ext import ApplicationBuilder
 from config import TOKEN
 from commands import register_handlers
 from core.queue import worker
-import asyncio
 
-async def post_init(application):
-    asyncio.create_task(worker())
 
-def main():
-    app = (
-        ApplicationBuilder()
-        .token(TOKEN)
-        .post_init(post_init)
-        .build()
-    )
+async def main():
+    app = ApplicationBuilder().token(TOKEN).build()
 
     register_handlers(app)
 
-    print("Bot rodando...")
-    app.run_polling()
+    # inicia worker após app inicializar
+    async with app:
+        asyncio.create_task(worker())
+        print("Bot rodando...")
+        await app.run_polling(drop_pending_updates=True)
+
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
