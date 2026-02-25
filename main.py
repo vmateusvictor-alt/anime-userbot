baaf7d2d4edb752b856f1239348ffec47afb9e2c
shadowsama41ns-cpu <shadowsama41ns@gmail.com>
2026-02-25 12:47:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,18 @@ from core.queue import worker
 import asyncio
 
 
-def main():
+async def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
     register_handlers(app)
 
-    # inicia worker quando o bot iniciar
-    async def on_startup(app):
-        asyncio.create_task(worker())
-
-    app.post_init = on_startup
-
     print("Bot rodando...")
 
-    app.run_polling(drop_pending_updates=True)
+    # inicia worker manualmente
+    asyncio.create_task(worker())
+
+    await app.run_polling(drop_pending_updates=True)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
