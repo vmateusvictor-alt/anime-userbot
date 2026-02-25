9e8521a9a9e5c131830f86dbab6b5d8d3d70916d
shadowsama41ns-cpu <shadowsama41ns@gmail.com>
2026-02-25 13:27:57 +0200

    Update queue.py

--- core/queue.py
+++ core/queue.py
@@ -3,11 +3,12 @@ import asyncio
 download_queue = asyncio.Queue()
 
 async def worker():
+    print("WORKER INICIADO")
     while True:
         job = await download_queue.get()
         try:
             await job()
         except Exception as e:
-            print("Erro:", e)
+            print("Erro no job:", e)
         finally:
             download_queue.task_done()
