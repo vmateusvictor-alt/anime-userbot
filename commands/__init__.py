b3bbbc1c4414db14a7ecdf3860d5d1ba0d4362a1
shadowsama41ns-cpu <shadowsama41ns@gmail.com>
2026-02-25 13:32:55 +0200

    Update __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,8 +1,4 @@
-from telegram.ext import CommandHandler
-from .download import download_command, cancel_command
-from .start import start_command
+from .download import register_download
 
-def register_handlers(app):
-    app.add_handler(CommandHandler("start", start_command))
-    app.add_handler(CommandHandler("download", download_command))
-    app.add_handler(CommandHandler("cancel", cancel_command))
+def register_handlers(client):
+    register_download(client)
