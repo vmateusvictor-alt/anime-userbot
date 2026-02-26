cf2979352c1aa41d2d0b88c814b225823f39834c
vmateusvictor-alt <vmateus.victor@gmail.com>
2026-02-26 10:23:00 +0200

    Update queue.py

--- core/queue.py
+++ core/queue.py
@@ -2,12 +2,14 @@ import asyncio
 
 download_queue = asyncio.Queue()
 
-async def worker():
+async def worker(user_client):
     print("WORKER INICIADO")
+
     while True:
         job = await download_queue.get()
+
         try:
-            await job()
+            await job(user_client)
         except Exception as e:
             print("Erro no job:", e)
         finally:
