87634afeb843bb822df5faa2d3c0dbd00bed0195
vmateusvictor-alt <vmateus.victor@gmail.com>
2026-02-26 09:53:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import asyncio
+import traceback
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 from config import API_ID, API_HASH, STRING_SESSION
 from commands import register_handlers
 from core.queue import worker
+
+
+async def safe_worker():
+    try:
+        await worker()
+    except Exception:
+        print("Erro no worker:")
+        traceback.print_exc()
 
 
 async def main():
@@ -13,7 +22,7 @@ async def main():
         API_HASH
     )
 
-    await client.connect()
+    await client.start()
 
     if not await client.is_user_authorized():
         raise Exception("STRING_SESSION inválida!")
@@ -22,10 +31,14 @@ async def main():
 
     register_handlers(client)
 
-    asyncio.create_task(worker())
+    asyncio.create_task(safe_worker())
 
     await client.run_until_disconnected()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except Exception:
+        print("ERRO FATAL:")
+        traceback.print_exc()
