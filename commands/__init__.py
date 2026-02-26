0fe568906e8d6dd0b591b64e88e951ac43f55023
vmateusvictor-alt <vmateus.victor@gmail.com>
2026-02-26 12:09:03 +0200

    Delete commands directory

--- commands/__init__.py
+++
@@ -1,4 +0,0 @@
-from .download import register_download
-
-def register_handlers(client):
-    register_download(client)
--- commands/start.py
+++
@@ -1,7 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "🤖 Bot online!\n\nUse:\n/download <url>"
-    )
