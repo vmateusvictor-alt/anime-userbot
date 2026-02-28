fa8b408a2b04c9e924e11a662129241722a6917d
vmateusvictor-alt <vmateus.victor@gmail.com>
2026-02-28 22:11:00 +0200

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,5 +1,6 @@
 import os
 import asyncio
+import time
 import yt_dlp
 
 DOWNLOAD_DIR = "downloads"
@@ -8,26 +9,42 @@ os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
 async def download_universal(url, progress_callback=None):
 
+    loop = asyncio.get_event_loop()
     output_template = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
 
-    loop = asyncio.get_event_loop()
+    last_percent = 0
+    last_time = 0
 
     def progress_hook(d):
+        nonlocal last_percent, last_time
+
         if d['status'] == 'downloading':
-            if progress_callback and d.get("total_bytes"):
+            if d.get("total_bytes"):
                 percent = (d["downloaded_bytes"] / d["total_bytes"]) * 100
-                asyncio.run_coroutine_threadsafe(
-                    progress_callback(percent),
-                    loop
-                )
+                now = time.time()
+
+                # Atualiza apenas se:
+                # - passou 5%
+                # - ou passou 3 segundos
+                if percent - last_percent < 5 and now - last_time < 3:
+                    return
+
+                last_percent = percent
+                last_time = now
+
+                if progress_callback:
+                    asyncio.run_coroutine_threadsafe(
+                        progress_callback(percent),
+                        loop
+                    )
 
     ydl_opts = {
         "outtmpl": output_template,
         "format": "bestvideo+bestaudio/best",
-        "merge_output_format": "mkv",  # mantém mkv se vier mkv
+        "merge_output_format": "mkv",
         "noplaylist": True,
         "progress_hooks": [progress_hook],
-        "concurrent_fragment_downloads": 5,
+        "concurrent_fragment_downloads": 1,
         "retries": 10,
         "fragment_retries": 10,
         "http_headers": {
@@ -45,6 +62,7 @@ async def download_universal(url, progress_callback=None):
     return filepath
 
 
+# Compatibilidade
 async def download_mp4(url, progress_callback=None):
     return await download_universal(url, progress_callback)
 
