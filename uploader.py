bf36c667fc0dd4fb1286d9e4f5e8719eae029bff
vmateusvictor-alt <vmateus.victor@gmail.com>
2026-02-28 22:17:28 +0200

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -1,8 +1,5 @@
 import os
-import time
-import asyncio
 import subprocess
-from pyrogram.errors import FloodWait
 
 STORAGE_CHANNEL_ID = os.getenv("STORAGE_CHANNEL_ID")
 
@@ -34,55 +31,9 @@ def get_video_info(video_path):
     return width, height, duration
 
 
-def generate_thumbnail(video_path):
-    thumb_path = video_path + ".jpg"
-
-    cmd = [
-        "ffmpeg",
-        "-i", video_path,
-        "-ss", "00:00:03",
-        "-vframes", "1",
-        thumb_path,
-        "-y"
-    ]
-
-    subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
-    return thumb_path
-
-
 async def upload_video(userbot, filepath, message):
 
     width, height, duration = get_video_info(filepath)
-    thumb = generate_thumbnail(filepath)
-
-    last_percent = 0
-    last_time = 0
-
-    async def progress(current, total):
-        nonlocal last_percent, last_time
-
-        percent = (current / total) * 100
-        now = time.time()
-
-        # Atualiza apenas se passou 5% OU 3 segundos
-        if percent - last_percent < 5 and now - last_time < 3:
-            return
-
-        last_percent = percent
-        last_time = now
-
-        bar = "█" * int(percent // 5)
-        bar = bar.ljust(20, "░")
-
-        try:
-            await message.edit_text(
-                f"📤 Enviando...\n[{bar}] {percent:.2f}%"
-            )
-        except FloodWait as e:
-            await asyncio.sleep(e.value)
-        except:
-            pass
 
     sent = await userbot.send_video(
         chat_id=STORAGE_CHANNEL_ID,
@@ -90,12 +41,7 @@ async def upload_video(userbot, filepath, message):
         duration=duration,
         width=width,
         height=height,
-        thumb=thumb,
-        supports_streaming=True,
-        progress=progress
+        supports_streaming=True
     )
-
-    if os.path.exists(thumb):
-        os.remove(thumb)
 
     return sent.id
