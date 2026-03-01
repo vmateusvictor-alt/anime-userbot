5d9b1bfa33695261c96ebfbd67e16ec01b282774
vmateusvictor-alt <vmateus.victor@gmail.com>
2026-03-01 20:17:51 +0200

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -2,10 +2,11 @@ import os
 import time
 import subprocess
 
-STORAGE_CHANNEL_ID = os.getenv("STORAGE_CHANNEL_ID")
+STORAGE_CHANNEL_ID = int(os.getenv("STORAGE_CHANNEL_ID"))
 
 
 def get_video_info(video_path):
+
     cmd = [
         "ffprobe",
         "-v", "error",
@@ -32,48 +33,26 @@ def get_video_info(video_path):
     return width, height, duration
 
 
-def generate_thumbnail(video_path):
-    thumb_path = video_path + ".jpg"
-
-    cmd = [
-        "ffmpeg",
-        "-i", video_path,
-        "-ss", "00:00:03",
-        "-vframes", "1",
-        thumb_path,
-        "-y"
-    ]
-
-    subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
-    return thumb_path
-
-
 async def upload_video(userbot, filepath, message):
 
     filename = os.path.basename(filepath)
-    thumb = generate_thumbnail(filepath)
     width, height, duration = get_video_info(filepath)
 
     last_percent = 0
-    last_edit_time = 0
+    last_time = 0
 
     async def progress(current, total):
-        nonlocal last_percent, last_edit_time
+        nonlocal last_percent, last_time
 
         percent = (current / total) * 100
         now = time.time()
 
-        if percent - last_percent >= 3 or (now - last_edit_time) > 2:
+        if percent - last_percent >= 10 or (now - last_time) > 3:
             last_percent = percent
-            last_edit_time = now
-
-            bar = "█" * int(percent // 5)
-            bar = bar.ljust(20, "░")
-
+            last_time = now
             try:
                 await message.edit_text(
-                    f"📤 Enviando...\n[{bar}] {percent:.2f}%"
+                    f"📤 Enviando... {percent:.0f}%"
                 )
             except:
                 pass
@@ -82,15 +61,11 @@ async def upload_video(userbot, filepath, message):
         chat_id=STORAGE_CHANNEL_ID,
         video=filepath,
         caption=filename,
-        thumb=thumb,
         duration=duration,
         width=width,
         height=height,
         supports_streaming=True,
         progress=progress
     )
-
-    if os.path.exists(thumb):
-        os.remove(thumb)
 
     return sent.id
